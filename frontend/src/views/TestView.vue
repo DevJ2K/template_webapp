69aa707780dc9874dd5d7c5ac49547105fb5fc23
theo <ajavontheopro@gmail.com>
2024-08-13 01:35:51 +0200

    ✅ : Test page setup in frontend

---
+++ frontend/src/views/TestView.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import TheWelcome from "../components/TheWelcome.vue";
+const makeRequests = async () => {
+  console.log("Yo !");
+};
+</script>
+
+<template>
+  <main class="flex h-screen items-center">
+    <button @click="makeRequests">Click me !</button>
+  </main>
+</template>
