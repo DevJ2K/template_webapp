463aa97b30b94ddcda7e3761934766ee4d1a7293
theo <ajavontheopro@gmail.com>
2024-08-13 01:53:26 +0200

    ✅ : Can make post request

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,14 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+
+class Test(BaseModel):
+     name: str
 
 app = FastAPI()
 
@@ -8,3 +18,17 @@ async def root():
 		"response": 200,
 		"message": "API is working !"
 	}
+
+@app.post("/items/")
+async def create_item(item: Item):
+    return item
+
+@app.post("/test")
+async def test_post(test: Test):
+     print(test)
+     return {
+          "response": 200,
+          "message": test.name
+	 }
+
+# curl -s -H 'Content-Type: application/json' -d '{ "name":"foo"}' -X POST http://127.0.0.1:8000/test/ | jq
