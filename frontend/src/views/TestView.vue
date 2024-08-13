aab836f8027686e7509483c538c0bf1728ed9582
theo <ajavontheopro@gmail.com>
2024-08-13 02:18:49 +0200

    ✅ : Can make post request from frontend

--- frontend/src/views/TestView.vue
+++ frontend/src/views/TestView.vue
@@ -2,6 +2,22 @@
 import TheWelcome from "../components/TheWelcome.vue";
 const makeRequests = async () => {
   console.log("Yo !");
+  const url = "http://127.0.0.1:8000/test";
+
+  try {
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({name: "test"}),
+        });
+
+        const result = await response.json();
+        console.log("Response from FastAPI:", result);
+    } catch (error) {
+        console.error("Error:", error);
+    }
 };
 </script>
 
