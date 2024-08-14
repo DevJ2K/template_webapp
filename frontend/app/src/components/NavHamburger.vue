d83b25647073ea9e1fdc07f991386a3ea995749d
DevJ2K <ajavontheopro@gmail.com>
2024-08-14 15:39:49 +0200

    ✅ : Move frontend/ -> frontend/app/.

---
+++ frontend/app/src/components/NavHamburger.vue
@@ -0,0 +1,13 @@
+<template>
+	<div class="flex size-fit items-center justify-center">
+
+		<div class="grid justify-items-center gap-1.5">
+			<div id="hamburger-bar-1" class="h-1 w-8 rounded-full bg-high-contrast-text transition-all duration-300 dark:bg-d-high-contrast-text"></div>
+			<div id="hamburger-bar-2" class="h-1 w-8 rounded-full bg-high-contrast-text transition-all duration-300 dark:bg-d-high-contrast-text"></div>
+			<div id="hamburger-bar-3" class="h-1 w-8 rounded-full bg-high-contrast-text transition-all duration-300 dark:bg-d-high-contrast-text"></div>
+		</div>
+	</div>
+</template>
+
+<script setup lang="ts">
+</script>
