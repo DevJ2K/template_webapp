3edbf3ed3312c51ec54d1a27b4dbd00fcfde4ade
DevJ2K <ajavontheopro@gmail.com>
2024-08-14 14:22:16 +0200

    ✅ : Connect API to HomeView.

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -26,14 +26,14 @@
       <div class="flex w-full flex-col items-center justify-center gap-3">
         <label for="test-input">Lorem ipsum :</label>
         <input type="text" id="test-input" required class="form-input form-input-border w-5/6">
-        <button
+        <button @click="makeRequests"
 							class="group flex flex-row items-center gap-1.5 rounded-full border-2 border-accent-color px-6 py-2 text-high-contrast-text transition-colors hover:bg-accent-color dark:text-d-high-contrast-text">
 							<span class=" font-semibold transition-colors group-hover:text-white">Submit</span>
 					</button>
       </div>
       <div>
         <h1 class=" text-xl font-semibold">API Result</h1>
-        
+
         <p id="api_result"></p>
       </div>
     </div>
@@ -42,4 +42,35 @@
 </template>
 
 <script setup lang="ts">
+
+const makeRequests = async () => {
+  console.log("Yo !");
+  const input: HTMLElement | null = document.getElementById("test-input");
+
+  let text: String = '';
+  if (input) {
+    text = input.value;
+  }
+  const url = "http://127.0.0.1:8000/test";
+
+  try {
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({name: text}),
+        });
+
+        const result = await response.json();
+        console.log("Response from FastAPI:", result);
+        const api_result = document.getElementById("api_result");
+        if (api_result) {
+          api_result.innerHTML = result.message;
+        }
+    } catch (error) {
+        console.error("Error:", error);
+    }
+};
+
 </script>
